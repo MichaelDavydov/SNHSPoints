593354bcc3ee9dfb577082f8ba2639223009c79d
MichaelDavydov <michaeladavydov@gmail.com>
2023-09-14 18:19:22 -0500

    Adds the activity list and stuff for it

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
-  const url ="https://docs.google.com/spreadsheets/d/1bPQBzX6MrLFHPIwsqV9Ipv_emW_Vg6UyPb-3w1ylWiE/gviz/tq?&gid=0";
+  const url =    "https://docs.google.com/spreadsheets/d/1bPQBzX6MrLFHPIwsqV9Ipv_emW_Vg6UyPb-3w1ylWiE/gviz/tq?&gid=0";
+  const logurl = "https://docs.google.com/spreadsheets/d/1bPQBzX6MrLFHPIwsqV9Ipv_emW_Vg6UyPb-3w1ylWiE/gviz/tq?&gid=1857132020";
 	const main = document.querySelector("main");
   const searchTerm = document.getElementById('searchBar');
   const answerField = document.getElementById('p');
+  const hoursList = document.getElementById('check');
   const semsStatus = document.getElementById('status');
 
   namesList = fetch(url)
@@ -10,6 +12,13 @@
       const data = JSON.parse(rep.substr(47).slice(0,-2));
       return data;
     });      
+
+  serviceList = fetch(logurl)
+    .then(res=>res.text())
+    .then(rep => {
+      const data = JSON.parse(rep.substr(47).slice(0,-2));
+      return data;
+    });  
 
 function search() {
   
@@ -56,10 +65,34 @@ namesList.then(loop => {
 
 } 
 
+function listService(){
 
+  let searchID = searchTerm.value;
+
+  let totalList = "<span style='font-family: Bebas Neue, cursive; font-size: 35px; color: lightgrey; text-decoration: underline;'>" + "        Activity List" + "</span>";
+
+  serviceList.then(loop => {
+        
+    for (let i = 1; i < loop.table.rows[0].c[10].v; i++){
+
+       if(searchID == loop.table.rows[i].c[1].v) {
+         
+         totalList = totalList + "<li>" + "<span style='color:white;'>" + loop.table.rows[i].c[4].v * 10 + " points " + "</span>" + "- " + loop.table.rows[i].c[3].v + " " + loop.table.rows[i].c[5].f + "</li>";
+
+            }
+            
+          }  
+
+          hoursList.innerHTML = totalList;
+     /* }).catch(err => {
+        hoursList.innerHTML = "error.";*/
+    }); 
+  }
     
-  //NamesList.then(print => console.log(print));
+ // hoursList.innerHTML = 'test' + "\n" + "did it work?";
+  serviceList.then(print => console.log(print.table)); // this works!
+  //serviceList.then(print => console.log("test" + print.table.rows[5].c[1].v + "\n" + "wtf"));
   //NamesList.then(print => console.log(print.table.rows[0].c[1].v));
   
-
+  //console.log(totalList);
                                    
